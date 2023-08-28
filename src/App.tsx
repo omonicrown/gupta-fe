486a79b1af00a080fac1fbbc78de6fea72e55528
omolade samuel <mac@omolades-MacBook-Pro.local>
2023-08-28 15:49:07 +0200

    commit

--- src/App.tsx
+++ src/App.tsx
@@ -4,10 +4,13 @@ import { Routes, Route } from "react-router-dom";
 import Register from './Components/Pages/Auth/Register'
 import Login from './Components/Pages/Auth/Login';
 import Index from './Components/Pages/Auth/index';
-import Dashboard from './Components/admin/Dashboard';
+import Dashboard from './Components/admin/TiredLink';
 import Mylinks from './Components/admin/MyLinks';
 import CreateLink from './Components/admin/CreateLink';
 import LinkDetails from './Components/admin/LinkDetails';
+import EditMultiLink from './Components/admin/EditMultiLink';
+import ViewMultiPage from './Components/admin/ViewMultiPage';
+import CreateMultiLink from './Components/admin/CreateMultiLink';
 import Formlink from './Components/admin/FormLinks';
 import Chatrouting from './Components/admin/ChatRouting';
 import EditUserMessage from './Components/admin/EditUserMessage';
@@ -31,11 +34,13 @@ function App() {
            {/* <Route  path="/login" element={<Login/>}/> */}
            <Route  path="/mylinks" element={<Mylinks/>}/>
            <Route  path="/createlink" element={<CreateLink/>}/>
-           <Route  path="/linkDetails/:linkId" element={<LinkDetails/>}/>
+           <Route  path="/update-multi-link/:linkId" element={<EditMultiLink/>}/>
+           <Route  path="/:linkId" element={<ViewMultiPage/>}/>
+           <Route  path="/create-multi-link" element={<CreateMultiLink/>}/>
            <Route  path="/EditUserMessage" element={<EditUserMessage/>}/>
            <Route  path="/editprofile" element={<EditProfile/>}/>
            <Route  path="/formlinks" element={<Formlink/>}/>
-           <Route  path="/tieredlinks" element={<Dashboard/>}/>
+           <Route  path="/multi-links" element={<Dashboard/>}/>
            <Route  path="/chatrouting" element={<Chatrouting/>}/>
            <Route  path='/sidebar' element={<SideBar/>}/>
            <Route  path='/proplan' element={<ProPlan/>}/>
